b8077b3221f18a3b766186ddb5c3118200f25a72
Chris Bigelow <8963042+chrisbigelow@users.noreply.github.com>
2025-12-13 10:24:32 -0700

    changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/static';
 
 export default defineConfig({
+  site: 'https://pelletsaunaco.com', // Update this with your actual domain
   output: 'static',
   adapter: vercel(),
 });
